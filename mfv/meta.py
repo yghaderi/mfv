a58ffb30dd8eb7fa6c345b1c763c1d1c35cecf1b
Yaghoub Ghadri <y.ghaderi@outlook.com>
2024-02-09 12:24:28 +0330

    add meta

---
+++ mfv/meta.py
@@ -0,0 +1,16 @@
+from typing import Literal
+from pydantic import BaseModel
+
+
+class InventoryManagement(BaseModel):
+    """
+    approach {
+        1: 'خریدِ کسری و فروشِ مازاد'
+        2: 'خریدِ کسری و عدمِ فروش'
+        3: 'عدمِ خرید و فروشِ مازاد'
+        4: 'عدمِ خرید و عدمِ فروش'
+        5: 'خریدِ کسری برای مصرف و فروشِ مازادِ حاصل از تولید'
+        6: 'محدودیتِ فروش-مصرف'
+        }
+    """
+    approach: Literal[1, 2, 3, 4, 5, 6]
